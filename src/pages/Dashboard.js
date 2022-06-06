23a21f31df9f7a7d15380bb67c1b462bc19f17e5
James OKeeffe <jamesokeeffe50@gmail.com>
2022-06-06 23:42:08 +0800

    Final styling changes

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -7,12 +7,12 @@ import { useSelector } from 'react-redux'
 export default function Dashboard() {
     const defaultTheme = useSelector(state => state.user.defaultTheme)
     return (
-        <>
+        <div className='container'>
           <NavBar />
           <div className={defaultTheme? 'page darkPage' : 'page lightPage'}>
             <SideBar />
             <AddToDo />
           </div>
-        </>
+        </div>
     )
 }
